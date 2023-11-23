c7029d255dea91f46e5883bd437170af97815c42
YaremaNatalia <yaremka.n@gmail.com>
2023-11-23 20:24:43 +0200

    add dotenv

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import logger from "morgan";
 import cors from "cors";
+import "dotenv/config"; //виклик методу конфігурації в dotenv який записує інф з файлу .env в глобальний обєкт process.env
 
 import contactsRouter from "./routes/api/contacts-router.js";
 import authRouter from "./routes/api/auth-router.js";
--- server.js
+++ server.js
@@ -3,13 +3,12 @@ import mongoose from "mongoose";
 
 // process.env налаштування сервера куди можна додати секретну інформацію (ключі, паролі)
 
-// const DB_HOST =
-//   "mongodb+srv://YaremaNata:o7pESaJvGKKiaesU@cluster0.gw3gcg9.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST, PORT } = process.env;
 
 mongoose
-  .connect(process.env.DB_HOST)
+  .connect(DB_HOST)
   .then(() => {
-    app.listen(3000, () => console.log("Database connection successful"));
+    app.listen(PORT, () => console.log("Database connection successful"));
   })
   .catch((error) => {
     console.log(error.message);
