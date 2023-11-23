a5a4d54d96ae21738b98901a52855a54dd347c28
Natalia Yarema <111983329+YaremaNatalia@users.noreply.github.com>
2023-11-23 20:54:12 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import app from "./app.js";
 import mongoose from "mongoose";
 
-const DB_HOST =
-  "mongodb+srv://YaremaNata:o7pESaJvGKKiaesU@cluster0.gw3gcg9.mongodb.net/db-contacts?retryWrites=true&w=majority";
+// const DB_HOST =
+//   "mongodb+srv://YaremaNata:o7pESaJvGKKiaesU@cluster0.gw3gcg9.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
 mongoose
-  .connect(DB_HOST)
+  .connect(process.env.DB_HOST)
   .then(() => {
     app.listen(3000, () => console.log("Database connection successful"));
   })
